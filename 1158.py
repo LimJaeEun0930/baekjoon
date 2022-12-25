117fa72b02a3a41dd3cfd6ef8cf90515c66bdb8f
LimJaeEun0930 <>
2022-12-25 19:03:35 +0900

    pop()은 리스트의 맨 마지막 요소를 리턴하고 그 요소는 삭제한다. %은 원형리스트?에서 다음 인덱스넘버가 리스트의 len보다 클 때 이를 대처하기 위해 쓰인다.

---
+++ 1158.py
@@ -0,0 +1,13 @@
+N, K = map(int, input().split())
+arr = [i for i in range(1, N + 1)]
+
+answer = []
+num = 0
+
+for t in range(N):
+    num += K - 1
+    if num >= len(arr): 
+        num = num % len(arr)
+
+    answer.append(str(arr.pop(num))) # pop(num)은 num을 리턴하고 num을 지운다.
+print("<", ", ".join(answer)[:], ">", sep='')
