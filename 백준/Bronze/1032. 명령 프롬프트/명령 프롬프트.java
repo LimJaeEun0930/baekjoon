d755c3d8222e071b2e1561a9b11ed7b5ee32cdf8
JaeEunLim <58625475+LimJaeEun0930@users.noreply.github.com>
2024-12-27 22:50:22 +0900

    [Bronze I] Title: 명령 프롬프트, Time: 104 ms, Memory: 14184 KB -BaekjoonHub

--- 백준/Bronze/1032. 명령 프롬프트/명령 프롬프트.java
+++ 백준/Bronze/1032. 명령 프롬프트/명령 프롬프트.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 
-public class Main {
+public class Main { // 비트연산을 이요한 풀이
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         int count = Integer.parseInt(bf.readLine());
@@ -11,13 +11,14 @@ public class Main {
         while (--count >= 0) {
             inputs[count] = bf.readLine();
         }
-        byte[] diffIndexes = new byte[inputs[0].length()];
+        long diffIndexes = 0;
         for (int i = 0; i < inputs[0].length(); ++i) {
             char ch = inputs[0].charAt(i);
             for (String s : inputs) {
                 char ch2 = s.charAt(i);
                 if (ch != ch2) {
-                    diffIndexes[i] = 1;
+                    diffIndexes += 1L << inputs[0].length() -1 - i;
+                    break;
                 }
                 ch = ch2;
             }
@@ -25,7 +26,7 @@ public class Main {
 
         StringBuilder sb = new StringBuilder(inputs[0].length());
         for (int i = 0; i < inputs[0].length(); ++i) {
-            if (diffIndexes[i] == 1) {
+            if ((diffIndexes & (1L << inputs[0].length() -1 - i)) != 0) {
                 sb.append('?');
             } else {
                 sb.append(inputs[0].charAt(i));
